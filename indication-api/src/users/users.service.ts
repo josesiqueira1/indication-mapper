a879327341297f8a8aa776a3f78b328da34a0392
José Siqueira <joseleonardosiqueira@gmail.com>
2025-04-12 16:59:51 -0300

    feat: duplicated email validation

--- indication-api/src/users/users.service.ts
+++ indication-api/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
@@ -7,9 +7,7 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectModel(User.name) private userModel: Model<UserDocument>,
-  ) {}
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
@@ -17,7 +15,16 @@ export class UsersService {
       ...createUserDto,
       password: hashedPassword,
     });
-    return createdUser.save();
+
+    try {
+      return createdUser.save();
+    } catch (err) {
+      // Duplicate key
+      if (err.code === 11000) {
+        throw new ConflictException('Email already in use');
+      }
+      throw err;
+    }
   }
 
   async findAll(): Promise<User[]> {
@@ -31,4 +38,4 @@ export class UsersService {
   async findByEmail(email: string): Promise<User | null> {
     return this.userModel.findOne({ email }).exec();
   }
-} 
+}
