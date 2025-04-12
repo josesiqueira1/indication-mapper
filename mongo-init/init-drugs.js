be2b974ebca3e75fc67ca95276112f63930274d2
José Siqueira <joseleonardosiqueira@gmail.com>
2025-04-12 17:52:13 -0300

    feat: swagger

--- mongo-init/init-drugs.js
+++ mongo-init/init-drugs.js
@@ -1,4 +1,4 @@
-db = db.getSiblingDB('indication-mapper'); // substitua pelo nome do seu banco
+db = db.getSiblingDB('indication-mapper');
 
 db.drugs.insertOne({
   setid: "595f437d-2729-40bb-9c62-c8ece1f82780",
